63d6202462f3f7e716b730c05bb95fe32425bc22
Daniel Gómez <gomez.leyva.daniel@gmail.com>
2024-08-26 18:27:59 -0600

    change to useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { useReducer } from "react"
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
 import OrderTotals from "./components/OrderTotals"
 import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db" 
-import  useOrder from './hooks/useOrder'
+import { initialState, orderReducer } from "./reducers/order-reducer"
 
 function App() {
-
-  const { order, addItem, removeItem,tip,setTip, placeOrder } = useOrder()
+  
+  const[state, dispatch] = useReducer(orderReducer, initialState)
 
   return (
     <>
@@ -25,7 +26,7 @@ function App() {
               <MenuItem
                 key={item.id}
                 item={item}
-                addItem={addItem}
+                dispatch={dispatch}
               />
             ))}
           </div>
@@ -33,22 +34,22 @@ function App() {
         </div>
 
         <div className=" border border-dashed border-blue-400 p-5 rounded-lg space-y-10">
-          {order.length ? (
+          {state.order.length ? (
             <>
               <OrderContents
-              order={order}
-              removeItem={removeItem}
+              order={state.order}
+              dispatch={dispatch}
               />
 
               <TipPercentageForm
-                setTip={setTip}
-                tip={tip}
+                dispatch={dispatch}
+                tip={state.tip}
               />
 
               <OrderTotals
-                order={order}
-                tip={tip}
-                placeOrder={placeOrder}
+                order={state.order}
+                tip={state.tip}
+                dispatch={dispatch}
               />
             </>
           ) : (<p className=" text-center">La orden esta vacía</p>) }
